3a259d92347d803080975e8ca99df684a9025c6c
Luis Guerrero <guerrerotook@outlook.com>
2020-03-21 16:48:08 +0000

    Solved an small bug on the test

--- tests/components/iberdrola/test_config_flow.py
+++ tests/components/iberdrola/test_config_flow.py
@@ -11,7 +11,7 @@ PASSWORD = "Password"
 _LOGGER = logging.getLogger(__name__)
 
 
-async def test_user(hass):
+async def test_async_step_user(hass):
     """Test user config."""
     flow = config_flow.IberdrolaConfigFlow()
     flow.hass = hass
@@ -28,3 +28,23 @@ async def test_user(hass):
         assert result["errors"]["base"] == "invalid_credentials"
     else:
         assert result["title"] == f"Iberdrola-{USERNAME}"
+
+
+async def test_async_step_import(hass):
+    """Test user config."""
+    flow = config_flow.IberdrolaConfigFlow()
+    flow.hass = hass
+
+    result = await flow.async_step_import(
+        {CONF_USERNAME: USERNAME, CONF_PASSWORD: PASSWORD, CONF_SCAN_INTERVAL: 10}
+    )
+    _LOGGER.info(result)
+    if USERNAME == "Username":
+        assert result["reason"] == "invalid_credentials"
+    else:
+        assert result["title"] == f"Iberdrola-{USERNAME} (from configuration)"
+
+
+async def test_configured_accounts(hass):
+    """Test user config."""
+    config_flow.configured_accounts(hass)
